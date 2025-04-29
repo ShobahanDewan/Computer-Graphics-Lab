8ec03269f089cd2c4592a57fb4c767094a715709
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-04-30 00:51:51 +0600

    Draw a Collorfull Triangle

---
+++ Draw a Collorfull Triangle.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Turtle()
+t.fillcolor("red")
+t.begin_fill() 
+t.forward(400)
+t.left(135)
+t.forward(283)
+t.left(90)
+t.forward(283)
+t.left(135)
+
+t.end_fill()
+turtle.mainloop()
