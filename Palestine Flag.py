853889d474e93e6872b3d4dfc464fb141e3a0f67
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-05-13 20:31:54 +0600

    Palestine Flag

--- Palestine Flag.py
+++ Palestine Flag.py
@@ -1,47 +1,52 @@
 import turtle
 
-# Set up screen
-screen = turtle.Screen()
-screen.title("Palestine Flag")
-screen.bgcolor("white")
+t = turtle.Turtle()
 
-pen = turtle.Turtle()
-pen.speed(0)
+t.color("black")
+t.begin_fill()
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.end_fill()
 
-# Function to draw rectangle
-def draw_rectangle(color, x, y, width, height):
-    pen.penup()
-    pen.goto(x, y)
-    pen.pendown()
-    pen.color(color)
-    pen.begin_fill()
-    for _ in range(2):
-        pen.forward(width)
-        pen.right(90)
-        pen.forward(height)
-        pen.right(90)
-    pen.end_fill()
+t.color("white")
+t.begin_fill()
+t.forward(80)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.end_fill()
 
-# Draw black stripe
-draw_rectangle("black", -150, 100, 300, 33.33)
+t.color("green")
+t.begin_fill()
+t.forward(80)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(80)
+t.end_fill()
+t.left(135)
 
-# Draw white stripe
-draw_rectangle("white", -150, 66.67, 300, 33.33)
+t.color("red")
+t.begin_fill()
+t.forward(170)
+t.left(90)
+t.forward(170)
+t.left(135)
+t.forward(240)
+t.end_fill()
 
-# Draw green stripe
-draw_rectangle("green", -150, 33.33, 300, 33.33)
-
-# Draw red triangle
-pen.penup()
-pen.goto(-150, 100)
-pen.pendown()
-pen.color("red")
-pen.begin_fill()
-pen.goto(-150, 0)
-pen.goto(-50, 50)
-pen.goto(-150, 100)
-pen.end_fill()
-
-# Hide turtle
-pen.hideturtle()
-turtle.done()
+turtle.mainloop()
